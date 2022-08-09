5401205341ace991e816fd2e29a9cf0936056517
HicksJonah <110473949+HicksJonah@users.noreply.github.com>
2022-08-09 11:07:47 -0500

    Initial commit

---
+++ lists.py
@@ -0,0 +1,7 @@
+names = ['shaggy', 'scooby', 'fred', 'daphne', 'velma']
+print(names)
+print(names[0].title())
+print(names[0])
+print(names[1])
+message_1 = "My favorite member of the Scooby Gang is " + names[0].title()
+print(message_1)
---
+++ name.cases.py
@@ -0,0 +1,19 @@
+name = "Jonah Hicks"
+print(name.upper())
+print(name.lower())
+first_name = "Jonah"
+last_name = "Hicks"
+full_name = first_name + " " + last_name
+print(full_name)
+print("Hello, " + full_name.title() + "!")
+message_1 = "Hello, " + full_name.title() + "!"
+print(message_1)
+name_2 = "Joe Celania"
+message_2 = name_2 + " is my best friend"
+print(message_2)
+print(name_2.upper())
+print(name_2.lower())
+print(name_2.title())
+name_3 = "Nolan Ryan"
+message_3 = name_3 + " once said, Enjoying success requires the ability to adapt. Only by being open to change will you have a true opportunity to get the most from your talent." 
+print(message_3)
