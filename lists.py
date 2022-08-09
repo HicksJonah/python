0ce6cc1cd90583e5f554c2abb711b5c3d6d233f1
HicksJonah <110473949+HicksJonah@users.noreply.github.com>
2022-08-09 17:32:18 -0500

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,7 +1,12 @@
 names = ['shaggy', 'scooby', 'fred', 'daphne', 'velma']
-print(names)
-print(names[0].title())
-print(names[0])
-print(names[1])
+# print(names)
+# print(names[0].title())
+# print(names[0])
+# print(names[1])
 message_1 = "My favorite member of the Scooby Gang is " + names[0].title()
-print(message_1)
+# print(message_1)
+#hello my name is jonah
+
+
+for name in names:
+    print(name)
