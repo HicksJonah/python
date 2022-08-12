a01b941cecd5defe03d2220b6d0ce8b2831823ae
HicksJonah <110473949+HicksJonah@users.noreply.github.com>
2022-08-12 10:46:37 -0500

    Update

---
+++ ch7.py
@@ -0,0 +1,11 @@
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
