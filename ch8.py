ca03dbe5b1c8fb6b1e152d7746bc530a95097496
HicksJonah <110473949+HicksJonah@users.noreply.github.com>
2022-08-18 15:55:23 -0500

    update

---
+++ ch8.py
@@ -0,0 +1,14 @@
+def greet_user(greeting):
+    """Display a simple greeting."""
+
+    print(greeting)
+greet_user("Hello!")
+
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+# describe_pet('hamster', 'harry')
+greet_user("Howdy!")
+greet_user("Ahoy!")
---
+++ ch9.py
@@ -0,0 +1,38 @@
+# class Dog(): 
+#     """A simple attempt to model a dog."""
+#     def __init__(self, name, age):
+#         """Initialize name and age attributes."""
+#         self.name = name
+#         self.age = age
+#     def sit(self):
+#         """Simulate a dog sitting in response to a command."""
+#         print(self.name.title() + " is now sitting.")
+#     def roll_over(self):
+#         """Simulate rolling over in response to a command."""
+#         print(self.name.title() + " rolled ever!")
+
+
+# my_dog = ('willie', 6)
+# print("My dog's name is " + my_dog.name.title() + ".")
+# print("My dog is " + str(my_dog.age) + " years old.")
+
+
+# class Car():
+#     """A simple attempt to represent a car."""
+# def __init__(self, make, model, year):
+#     """Initialize attributes to describe a car."""
+#     self.make = make
+#     self.model = model
+#     self.year = year
+# def get_descriptive_name(self):
+#     """Return a neatly formatted descriptive name."""
+#     long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+#     return long_name.title()
+# my_new_car = Car('audi', 'a4', 2016)
+# print(my_new_car.get_descriptive_name())
+
+
+my_dog = ('willie', 6)
+your_dog = ('lucy', 3)
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " + str(my_dog.age) + " years old.")
